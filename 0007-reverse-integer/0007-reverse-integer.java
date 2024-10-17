1fe0a3734872f1576aeb012f23a2415e69e33c6a
Sidhant  Rajput <151880795+sidhantrajput1@users.noreply.github.com>
2024-10-17 19:38:53 +0530

    Time: 8 ms (6.35%), Space: 40.3 MB (96.78%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -3,13 +3,23 @@ class Solution {
         int reverse = 0;
         while(x != 0) {
             int remender = x % 10;
-            if(reverse > Integer.MAX_VALUE/10 || reverse < Integer.MIN_VALUE/10) {
+            if(reverse > Integer.MAX_VALUE/10 || (reverse ==  Integer.MAX_VALUE/10 && remender > 7)) {
+                return 0;
+            }
+            if(reverse < Integer.MIN_VALUE/10 || (reverse ==  Integer.MIN_VALUE/10 && remender < -8)) {
                 return 0;
             }
             reverse = reverse * 10 + remender;
             x = x / 10;
             
         }
+
+        System.out.println("Max val is:  "+Integer.MAX_VALUE) ;
+        System.out.println("Min val is:  "+Integer.MIN_VALUE) ;
+
+        
+
+
         return reverse;
     }
     public static void main(String[] args) {
