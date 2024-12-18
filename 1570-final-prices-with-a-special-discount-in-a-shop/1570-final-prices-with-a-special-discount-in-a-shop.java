b3c6e1f6e3a1ed6aa25e8d2ea8372b5753383959
Sidhant  Rajput <151880795+sidhantrajput1@users.noreply.github.com>
2024-12-19 00:38:35 +0530

    Time: 2 ms (74.76%), Space: 43.4 MB (90.77%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
@@ -3,6 +3,8 @@ class Solution {
         int n = prices.length;
         int[] answer = new int[n];
 
+        answer[n-1] = prices[n-1];
+
         for(int i = 0; i < n; i++) {
             for(int j = i + 1 ; j < n; j++) {
                 if(j > i && prices[j] <= prices[i]) {
@@ -11,9 +13,6 @@ class Solution {
                 } else {
                     answer[i] = prices[i];
                 }
-            }
-            if(i == n - 1) {
-                answer[i] = prices[i];
             }
         }
         return answer;
