a23b35d3792b0be5cfbadea3b62ab6a67deb4385
Sidhant  Rajput <151880795+sidhantrajput1@users.noreply.github.com>
2024-12-27 07:14:43 +0530

    Time: 0 ms (100%), Space: 11.9 MB (23.08%) - LeetHub

--- 1802-number-of-students-unable-to-eat-lunch/1802-number-of-students-unable-to-eat-lunch.cpp
+++ 1802-number-of-students-unable-to-eat-lunch/1802-number-of-students-unable-to-eat-lunch.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        // create a queue
         queue<int> q;
         int n = students.size();
         // student array elements push on queue 
         for(int i = 0 ; i < n; i++) {
-            q.push(students[i]);
+            q.push(students[i]); // [1,1,1,0,0,1]
         }
 
         int i = 0; // traverse the sandwiches array
-        int count = 0;
-        while(q.size() > 0 && count != q.size()) {
+        int count = 0; // count the each step of between student and sandwiches
+        while(q.size() > 0 && count != q.size()) { 
             if(q.front() == sandwiches[i]) {
                 count = 0;
                 q.pop();
