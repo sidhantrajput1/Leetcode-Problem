3e1de7ea5d56510cc4b530b3753bcc3bba0f1979
Sidhant  Rajput <151880795+sidhantrajput1@users.noreply.github.com>
2025-01-15 22:07:07 +0530

    Time: 0 ms (100%), Space: 16.5 MB (82.23%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -1,14 +1,3 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     // find the level fo tree
@@ -16,31 +5,22 @@ public:
         if(root==NULL) return 0;
         return 1 + max(levels(root->left) , levels(root->right));
     }
-
-    void printNthLevelOfElement(TreeNode* root, int curr, int level, vector<int>& v) {
-        if(root==NULL) return;
-        if(curr == level) {
-            v.push_back(root->val);
-            return;
-        }
-        printNthLevelOfElement(root->left, curr+1, level, v);
-        printNthLevelOfElement(root->right, curr+1, level, v);
-    }
-
     // bfs traversal
-    void leveltraversal(TreeNode* root, vector<vector<int>>& ans) {
-        int n = levels(root);
-        for(int i = 1; i <= n; i++) {
-            vector<int> v;
-            printNthLevelOfElement(root, 1, i, v);
-            ans.push_back(v);
-            cout<<endl;
-        }
+    void leveltraversal(TreeNode* root, vector<vector<int>>& ans, int level) {
+        if(root==NULL) return;
+        ans[level].push_back(root->val);
+        leveltraversal(root->left, ans, level+1);
+        leveltraversal(root->right, ans, level+1);
     }
 
     vector<vector<int>> levelOrder(TreeNode* root) {
+        int n = levels(root);
         vector<vector<int>> ans;
-        leveltraversal(root, ans);
+        for(int i = 1; i <= n; i++) {
+            vector<int> v;
+            ans.push_back(v);
+        }
+        leveltraversal(root, ans, 0);
         return ans;
     }
 };
