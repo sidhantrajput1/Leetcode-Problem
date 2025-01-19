1a018c7119b39d693519d582cd4b92bc3e36069e
Sidhant  Rajput <151880795+sidhantrajput1@users.noreply.github.com>
2025-01-19 20:45:31 +0530

    Time: 2 ms (6.84%), Space: 11 MB (66.44%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -19,7 +19,16 @@ public:
     }
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> ans;
-        solve(root, ans);
+        stack<TreeNode*> st;
+        if(root!=NULL) st.push(root);
+        while(st.size() > 0) {
+            TreeNode* temp = st.top();
+            st.pop();
+            ans.push_back(temp->val);
+            if(temp->left) st.push(temp->left);
+            if(temp->right) st.push(temp->right);
+        }
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
