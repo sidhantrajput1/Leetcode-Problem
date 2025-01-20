021a98f41fae669f34c55173c1bc76191e3fee8b
Sidhant  Rajput <151880795+sidhantrajput1@users.noreply.github.com>
2025-01-20 12:44:01 +0530

    Time: 0 ms (100%), Space: 17.2 MB (15.73%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    // find the level fo tree
-    int levels(TreeNode* root) {
-        if(root==NULL) return 0;
-        return 1 + max(levels(root->left) , levels(root->right));
-    }
-    // bfs traversal
-    void leveltraversal(TreeNode* root, vector<vector<int>>& ans, int level) {
-        if(root==NULL) return;
-        ans[level].push_back(root->val);
-        leveltraversal(root->left, ans, level+1);
-        leveltraversal(root->right, ans, level+1);
-    }
-
     vector<vector<int>> levelOrder(TreeNode* root) {
-        int n = levels(root);
         vector<vector<int>> ans;
-        for(int i = 1; i <= n; i++) {
-            vector<int> v;
-            ans.push_back(v);
+        if(root==NULL) return ans;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(q.size() > 0) {
+            int level = q.size();
+            vector<int> res;
+            for(int i = 0; i < level; i++) {
+                TreeNode* temp = q.front();
+                q.pop();
+                res.push_back(temp->val);
+                if(temp->left != NULL) q.push(temp->left);
+                if(temp->right != NULL) q.push(temp->right);
+            }
+            ans.push_back(res);
         }
-        leveltraversal(root, ans, 0);
         return ans;
     }
 };
