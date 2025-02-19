89df5f7d878d7c9192def4f769bc359ba591a1d3
Sidhant  Rajput <151880795+sidhantrajput1@users.noreply.github.com>
2025-02-19 09:18:21 +0530

    Time: 6 ms (12.86%), Space: 45.8 MB (7.2%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -2,9 +2,7 @@ class Solution {
     public int missingNumber(int[] nums) {
         Arrays.sort(nums);
         for(int i = 0; i < nums.length; i++) {
-            if(nums[i] != i) {
-                return i;
-            }
+            if(nums[i] != i) return i;
         }
         return nums.length;
     }
