e0283b19d2dc7618fb1ea56495669bea3a8b8b0c
Sidhant  Rajput <151880795+sidhantrajput1@users.noreply.github.com>
2025-03-04 01:01:48 +0530

    Time: 4 ms (80.6%), Space: 127.7 MB (77.25%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -4,6 +4,7 @@ public:
         vector<int> res(nums.size());
         int low = 0;
 
+        // first pass : if element less than pivot element 
         for(int i = 0; i < nums.size(); i++) {
             if(nums[i] < pivot) {
                 res[low] = nums[i];
@@ -11,7 +12,7 @@ public:
             }
         }
 
-        // second pass
+        // second pass : if element equal to pivot element
         for(int i = 0; i < nums.size(); i++) {
             if(nums[i] == pivot) {
                 res[low] = nums[i];
@@ -19,7 +20,7 @@ public:
             }
         }
 
-        // third pass
+        // third pass : if element greater to pivot element 
         for(int i = 0; i < nums.size(); i++) {
             if(nums[i] > pivot ){
                 res[low] = nums[i];
