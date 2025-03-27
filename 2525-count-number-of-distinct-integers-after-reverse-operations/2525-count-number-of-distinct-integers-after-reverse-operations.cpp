2baf3503b8bf3879539937abcc0eededef2da782
Sidhant  Rajput <151880795+sidhantrajput1@users.noreply.github.com>
2025-03-27 05:46:04 +0530

    Time: 179 ms (69.44%), Space: 110.6 MB (52.99%) - LeetHub

--- 2525-count-number-of-distinct-integers-after-reverse-operations/2525-count-number-of-distinct-integers-after-reverse-operations.cpp
+++ 2525-count-number-of-distinct-integers-after-reverse-operations/2525-count-number-of-distinct-integers-after-reverse-operations.cpp
@@ -3,8 +3,8 @@ public:
     int reverse(int n) {
         int rev = 0;
         while(n > 0) {
-            rev *= 10;
-            rev += (n % 10);
+            rev = rev * 10;
+            rev = rev + (n % 10);
             n /= 10;  
         }
         return rev;
