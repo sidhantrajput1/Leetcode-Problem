db2cac73e14946f8102b72e951f0699800b9bf27
Sidhant  Rajput <151880795+sidhantrajput1@users.noreply.github.com>
2025-03-27 08:02:46 +0530

    Time: 11 ms (6.1%), Space: 25.8 MB (30.74%) - LeetHub

--- 2847-find-maximum-number-of-string-pairs/2847-find-maximum-number-of-string-pairs.cpp
+++ 2847-find-maximum-number-of-string-pairs/2847-find-maximum-number-of-string-pairs.cpp
@@ -6,18 +6,11 @@ public:
         unordered_set<string> s;
 
         for (int i = 0; i < n; i++) {
-            s.insert(nums[i]);
-        }
-
-        for (int i = 0; i < n; i++) {
             string rev = nums[i];
             reverse(rev.begin(), rev.end());
 
-            if (rev == nums[i]) continue;
-            if ( s.find(rev) != s.end() ) {
-                count++;
-                s.erase(nums[i]);
-            }
+            if (s.find(rev) != s.end()) count++;
+            else s.insert(nums[i]);
         }
         return count;
     }
