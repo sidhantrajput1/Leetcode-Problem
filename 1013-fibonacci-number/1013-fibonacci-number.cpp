9caa7bab3da9d0ff7311113affc6d5568d054d87
Sidhant  Rajput <151880795+sidhantrajput1@users.noreply.github.com>
2025-06-24 11:20:47 +0530

    Time: 3 ms (66.37%), Space: 8.2 MB (11.08%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,7 +1,12 @@
 class Solution {
 public:
+    int fibo(int n, vector<int>& dp) {
+        if (n <= 1) return n;
+        if (dp[n] != -1) return dp[n];
+        return dp[n] = fibo(n-1, dp) + fibo(n-2, dp);
+    }
     int fib(int n) {
-        if (n == 0 || n == 1) return n;
-        return (fib(n-1) + fib(n-2));
+        vector<int> dp(n+1, -1);
+        return fibo(n, dp);
     }
 };
