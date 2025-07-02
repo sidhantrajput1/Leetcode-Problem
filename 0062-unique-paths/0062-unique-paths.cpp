8d81237f4dcb64c965a079c70b4df7afb4fb712f
Sidhant  Rajput <151880795+sidhantrajput1@users.noreply.github.com>
2025-07-02 20:44:47 +0530

    Time: 1 ms (25.24%), Space: 9.3 MB (33.73%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
+    // sr = starting row , sc = starting col, er = ending row and ec = ending col
     int helper(int sr, int sc, int er, int ec, vector<vector<int>>& dp) {
         if (sr == er && sc == ec) return 1;
+        // if starting row > ending row || starting col > ending col then always return 0
         if (sr > er || sc > ec) return 0;
 
         if (dp[sr][sc] != -1) return dp[sr][sc];
@@ -11,6 +13,8 @@ public:
 
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m, vector<int>(n, -1));
+        // starting colum 0 and starting row is also 0. 
+        // robot tries to move to the bottom-right corner grid[m - 1][n - 1] 
         return helper(0, 0, m - 1, n - 1, dp);
     }
 };
