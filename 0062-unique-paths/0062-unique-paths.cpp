1dc68fdb584b992117791822b769a66e0787b611
Sidhant  Rajput <151880795+sidhantrajput1@users.noreply.github.com>
2025-07-02 21:05:30 +0530

    Time: 0 ms (100%), Space: 7.8 MB (96.56%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
-    // sr = starting row , sc = starting col, er = ending row and ec = ending col
-    int helper(int sr, int sc, int er, int ec, vector<vector<int>>& dp) {
-        if (sr == er && sc == ec) return 1;
-        // if starting row > ending row || starting col > ending col then always return 0
-        if (sr > er || sc > ec) return 0;
-
-        if (dp[sr][sc] != -1) return dp[sr][sc];
-        
-        return dp[sr][sc] = helper(sr + 1, sc, er, ec, dp) + helper(sr, sc + 1, er, ec, dp);
-    }
-
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        // starting colum 0 and starting row is also 0. 
-        // robot tries to move to the bottom-right corner grid[m - 1][n - 1] 
-        return helper(0, 0, m - 1, n - 1, dp);
+        int dp[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i==0 || j==0) dp[i][j] = 1;
+                else dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
