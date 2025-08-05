0afe0a51eae62b902a0ef1e043356a7e4608a722
Sidhant  Rajput <151880795+sidhantrajput1@users.noreply.github.com>
2025-08-05 07:43:53 +0530

    Time: 0 ms (100%), Space: 10.8 MB (22.56%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,15 +3,15 @@ public:
     vector<int> dp;
     int helper(vector<int>& nums) {
         int n = nums.size();
-        dp.clear();
-        dp.resize(n);
+        if (n == 0) return 0;
         if (n == 1) return nums[0];
 
+        vector<int> dp(n);
         dp[n - 1] = nums[n - 1];
-        dp[n - 2] = max(nums[n-1], nums[n-2]);
+        dp[n - 2] = max(nums[n - 2], nums[n - 1]);
 
-        for (int i = n - 3; i >= 0; i--) {
-            dp[i] = max(nums[i] + dp[i + 2], 0 + dp[i + 1]);
+        for (int i = n - 3; i >= 0; --i) {
+            dp[i] = max(nums[i] + dp[i + 2], dp[i + 1]);
         }
 
         return dp[0];
