8c5942a564aeecf3a2aa2a009725d1549cd91803
Sidhant  Rajput <151880795+sidhantrajput1@users.noreply.github.com>
2025-08-05 13:23:19 +0530

    Time: 362 ms (5%), Space: 301 MB (5.04%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
-    vector<int>*coin;
     vector<int> dp;
  
     Solution() {
         dp.resize(1000006, -1);
     }
 
-    int helper(int amount) {
+    int helper(vector<int>& coins, int amount) {
         if (amount == 0) return 0;
         if (amount < 0) return INT_MAX;
 
         if (dp[amount] != -1) return dp[amount];
 
         int result = INT_MAX;
-        for (int i = 0; i < coin->size(); i++) {
-            int subAns = helper(amount - (*coin)[i]);
+        for (int coin : coins) {
+            int subAns = helper(coins, amount - coin);
             if (subAns != INT_MAX)
                 result = min(result, 1 + subAns);
         }
 
         return dp[amount] = result;
     }
-    // vector<int> dp;
     int coinChange(vector<int>& coins, int amount) {
-        // dp.clear();
-        // dp.resize();
-        coin = &coins;
-        int ans = helper(amount);
+        int ans = helper(coins, amount);
         return ans == INT_MAX ? -1 : ans;
     }
 };
