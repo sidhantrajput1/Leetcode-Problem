ec92b844093a67976066b57bcbfb10f77844b3b6
Sidhant  Rajput <151880795+sidhantrajput1@users.noreply.github.com>
2025-08-12 13:18:49 +0530

    Time: 4 ms (16.66%), Space: 16.2 MB (61.89%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
-    int helper(int sr, int sc, int er, int ec, vector<vector<int>>& nums, vector<vector<int>>& dp) {
-        if (sr > er || sc > ec) return INT_MAX;
-        if (sr == er && sc == ec) return nums[sr][sc];
-
-        if (dp[sr][sc] != -1) return dp[sr][sc];
-
-        int rightWays = helper(sr, sc+1, er, ec, nums, dp);
-        int bottomWays = helper(sr+1, sc , er, ec, nums, dp);
-        
-        dp[sr][sc] = nums[sr][sc] + min(rightWays, bottomWays);
-
-        return dp[sr][sc];
-    }
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
-        int n = grid[0].size(); 
+        int n = grid[0].size();
 
-        vector<vector<int>> dp(m, vector<int>(n, -1));   
-        
-        return helper(0, 0, m-1, n-1, grid, dp);
+        vector<vector<int>> ans(m, vector<int>(n));
+
+        // fill ans[0][0] = grid[0][0]
+        ans[0][0] = grid[0][0];
+
+        // fill the first col
+        for(int col = 1; col < n; col++) {
+            ans[0][col] = grid[0][col] + ans[0][col-1];
+        }
+
+        // fill the first row
+        for (int row = 1; row < m; row++) {
+            ans[row][0] = grid[row][0] + ans[row-1][0];
+        }
+
+        // fill the remaning
+        for(int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                ans[i][j] = grid[i][j] + min(ans[i-1][j], ans[i][j-1]);
+            }
+        }
+
+        return ans[m-1][n-1];
     }
 };
