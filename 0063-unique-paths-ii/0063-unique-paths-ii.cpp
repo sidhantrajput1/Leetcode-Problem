b4ae64b0cdca5c3ca1ca1241dd382a229496422c
Sidhant  Rajput <151880795+sidhantrajput1@users.noreply.github.com>
2025-08-12 11:43:06 +0530

    Time: 0 ms (100%), Space: 11.8 MB (54.58%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int helper(int sr, int sc, int er, int ec,vector<vector<int>>& nums, vector<vector<int>>& dp) {
+        if (sr > er || sc > ec) return 0;
+        if (sr == er && sc == ec) return 1;
+        
+        // obstacle check
+        if (nums[sr][sc] == 1 || nums[er][ec] == 1) return 0;
+
+        if (dp[sr][sc] != -1) return dp[sr][sc];
+        
+        int downWays = helper(sr+1, sc, er, ec, nums, dp);
+        int rightWays = helper(sr, sc+1, er, ec, nums, dp);
+        
+        return dp[sr][sc] = downWays + rightWays;
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();    
+        
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) return 0;
+
+        return helper(0, 0, m-1, n-1, obstacleGrid, dp);
+    }
+};
