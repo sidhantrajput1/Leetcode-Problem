123075090ae04fb62dda7e14dd1e58028ef6b9ef
Sidhant  Rajput <151880795+sidhantrajput1@users.noreply.github.com>
2025-08-14 21:10:29 +0530

    Time: 23 ms (83.86%), Space: 23 MB (38.55%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -2,56 +2,34 @@ class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
         int rows = grid.size();
+        if (rows == 0) return 0;
         int cols = grid[0].size();
 
-        int cc = 0; // store the number of connected component
+        int cc = 0; // number of connected components
+        vector<pair<int,int>> directions = {{-1,0},{1,0},{0,-1},{0,1}};
 
-        for(int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) 
-            {
-                // it is water body
-                if (grid[r][c] == '0') continue;
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c] == '1') { // found new unvisited land
+                    cc++;
+                    grid[r][c] = '0'; // mark visited
 
-                // new unvisited land found i.e new connected component found
-                cc++;
+                    queue<pair<int, int>> qu;
+                    qu.push({r, c});
 
-                // mark visited
-                grid[r][c] = '0';
+                    while (!qu.empty()) {
+                        auto [currRow, currCol] = qu.front();
+                        qu.pop();
 
-                // apply bfs
-                queue<pair<int, int> > qu;
-                qu.push({r, c});
+                        for (auto [dr, dc] : directions) {
+                            int nr = currRow + dr;
+                            int nc = currCol + dc;
 
-                while (!qu.empty()) {
-                    auto curr = qu.front(); // get one node from queue
-                    qu.pop();
-
-                    // go to all unvisited neighbour curr node
-                    int currRow = curr.first;
-                    int currCol = curr.second;
-
-                    // check up
-                    if (currRow - 1 >= 0 && grid[currRow - 1][currCol] == '1') {
-                        qu.push({currRow - 1, currCol});
-                        grid[currRow - 1][currCol] = '0';
-                    }
-
-                    // check down
-                    if (currRow + 1 < rows && grid[currRow + 1][currCol] == '1') {
-                        qu.push({currRow + 1, currCol});
-                        grid[currRow + 1][currCol] = '0';
-                    }
-
-                    // check left
-                    if (currCol - 1 >= 0 && grid[currRow][currCol - 1] == '1') {
-                        qu.push({currRow, currCol - 1});
-                        grid[currRow][currCol - 1] = '0';
-                    }
-
-                    // check up
-                    if (currCol + 1 < cols && grid[currRow][currCol + 1] == '1') {
-                        qu.push({currRow, currCol + 1});
-                        grid[currRow][currCol + 1] = '0';
+                            if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] == '1') {
+                                qu.push({nr, nc});
+                                grid[nr][nc] = '0'; // mark visited
+                            }
+                        }
                     }
                 }
             }
