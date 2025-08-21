ae8c6ce0168b8e2902682bab0bd290009b3149c5
Sidhant  Rajput <151880795+sidhantrajput1@users.noreply.github.com>
2025-08-21 22:11:59 +0530

    Time: 11 ms (29.81%), Space: 13.2 MB (47.36%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    int m , n;
-    int dp[502][502];
-    int helper(string s1, string s2, int i, int j) {
-        // if any one string is empty then you need to return another string length, so if ( i == 0 ) return remaing string s2 length as well as for s1
-        if ( i == 0 ) return j;  // insert all remaining of s2
-        if ( j == 0 ) return i;  // insert all remaining of s1
+    int minDistance(string s1, string s2) {
 
-        if (dp[i][j] != -1) return dp[i][j];
-        
-        if (s1[i-1] == s2[j-1]) {
-            return dp[i][j] = helper(s1, s2, i - 1, j - 1);
+        int m = s1.size()  , n = s2.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+        // base case
+        for (int i = 0; i <= m; i++) dp[i][0] = i;
+        for (int j = 0; j <= n; j++) dp[0][j] = j;
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1[i-1] == s2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = 1 + min({dp[i][j-1], dp[i-1][j], dp[i-1][j-1]});
+                }
+            }
         }
 
-        return dp[i][j] = 1 + min({helper(s1, s2, i, j - 1), 
-        helper(s1, s2, i - 1, j) , helper(s1, s2, i - 1, j - 1)});
-    }
-    int minDistance(string s1, string s2) {
-        m = s1.size(), n = s2.size();
-        memset(dp, -1, sizeof(dp));
-        return helper(s1, s2, m, n);
+        return  dp[m][n];
+        
     }
 };
