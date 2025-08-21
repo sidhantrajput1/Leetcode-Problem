cf911e83f36a97a094901e600f2343296fd293cf
Sidhant  Rajput <151880795+sidhantrajput1@users.noreply.github.com>
2025-08-21 16:45:39 +0530

    Time: 11 ms (90.07%), Space: 8.9 MB (95.01%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -4,21 +4,21 @@ public:
         int m = s.size();
         int n = t.size();
 
-        vector<vector<double>> dp(m + 1, vector<double>(n + 1, 0));
+        vector<double> prev(n + 1, 0), curr(n + 1, 0);
 
-        for (int i = 0; i <= m; i++) dp[i][0] = 1;
-
+        prev[0] = curr[0] = 1; // empty t can always be formed
 
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 if (s[i-1] == t[j-1]) {
-                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                    curr[j] = prev[j-1] + prev[j];
                 } else {
-                    dp[i][j] = dp[i-1][j];
+                    curr[j] = prev[j];
                 }
             }
+            prev = curr;
         }
 
-        return (int)dp[m][n];
+        return (int)prev[n];
     }
 };
