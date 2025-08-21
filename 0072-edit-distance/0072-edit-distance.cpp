2a1bc0c23c2cb134e0579bc5b2ab350aab6cea11
Sidhant  Rajput <151880795+sidhantrajput1@users.noreply.github.com>
2025-08-21 20:43:09 +0530

    Time: 95 ms (5.02%), Space: 79.3 MB (13.3%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,40 +1,24 @@
 class Solution {
 public:
     int m , n;
-    int dp[501][501];
+    int dp[502][502];
     int helper(string s1, string s2, int i, int j) {
-        if (i == m) return n - j;
-        if (j == n) return m - i;
+        // if any one string is empty then you need to return another string length, so if ( i == 0 ) return remaing string s2 length as well as for s1
+        if ( i == 0 ) return j;  // insert all remaining of s2
+        if ( j == 0 ) return i;  // insert all remaining of s1
 
         if (dp[i][j] != -1) return dp[i][j];
         
-        if (s1[i] == s2[j]) {
-            return dp[i][j] = helper(s1, s2, i + 1, j + 1);
+        if (s1[i-1] == s2[j-1]) {
+            return dp[i][j] = helper(s1, s2, i - 1, j - 1);
         }
 
-        // insert
-        // int insert = helper(s1, s2, i, j + 1);
-        // // delete
-        // int delet = helper(s1, s2, i + 1, j);
-        // // replace
-        // int replace = helper(s1, s2, i + 1, j + 1);
-
-        // int replace = 0;
-        // int delet = 0;
-        // int insert = 0;
-
-        // if (s1[i] != s2[j]) {
-        //     insert = helper(s1, s2, i, j + 1);
-        //     delet = helper(s1, s2, i + 1, j);
-        //     replace = helper(s1, s2, i + 1, j + 1);
-        // }
-
-        return dp[i][j] = 1 + min({helper(s1, s2, i, j + 1), 
-        helper(s1, s2, i + 1, j) , helper(s1, s2, i + 1, j + 1)});
+        return dp[i][j] = 1 + min({helper(s1, s2, i, j - 1), 
+        helper(s1, s2, i - 1, j) , helper(s1, s2, i - 1, j - 1)});
     }
     int minDistance(string s1, string s2) {
         m = s1.size(), n = s2.size();
         memset(dp, -1, sizeof(dp));
-        return helper(s1, s2, 0, 0);
+        return helper(s1, s2, m, n);
     }
 };
