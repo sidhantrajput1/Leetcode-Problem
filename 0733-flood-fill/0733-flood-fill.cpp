16f14c81f4cbfad9a7059af3fa20b2e0042f8c29
Sidhant  Rajput <151880795+sidhantrajput1@users.noreply.github.com>
2025-08-26 18:14:30 +0530

    Time: 0 ms (100%), Space: 18 MB (76.68%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
+    int m, n;
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        m = image.size();
+        n = image[0].size();
+
         int initialColor = image[sr][sc];
+
         if (initialColor == color) return image;
+
         dfs(image, sr, sc, initialColor, color);
+
         return image;
     }
 
-    void dfs(vector<vector<int> >& image, int currRow, int currCol, int initialColor, int newColor) {
-        int n = image.size(); // total rows
-        int m = image[0].size(); // total Cols
-
-        if (currRow < 0 || currCol < 0 || currRow >= n || currCol >= m) {
+    void dfs(vector<vector<int>>& image, int currRow, int currCol, int initialColor, int newColor) {
+        if (currRow < 0 || currCol < 0 || currRow >= m || currCol >= n) {
             return;
         }
 
         if (image[currRow][currCol] != initialColor) return;
 
+
         image[currRow][currCol] = newColor;
-        dfs(image, currRow, currCol-1, initialColor, newColor); // left
-        dfs(image, currRow, currCol+1, initialColor, newColor); // right
-        dfs(image, currRow-1, currCol, initialColor, newColor); // up
-        dfs(image, currRow+1, currCol, initialColor, newColor); // down
+
+        dfs(image, currRow, currCol - 1, initialColor, newColor); // left
+        dfs(image, currRow, currCol + 1, initialColor, newColor); // right
+        dfs(image, currRow - 1, currCol, initialColor, newColor); // up
+        dfs(image, currRow + 1, currCol, initialColor, newColor); // down
     }
 };
