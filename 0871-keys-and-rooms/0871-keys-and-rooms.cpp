239272e46115b335bf07cec6215c29ac02352440
Sidhant  Rajput <151880795+sidhantrajput1@users.noreply.github.com>
2025-08-27 18:30:15 +0530

    Time: 0 ms (100%), Space: 14.8 MB (33.59%) - LeetHub

--- 0871-keys-and-rooms/0871-keys-and-rooms.cpp
+++ 0871-keys-and-rooms/0871-keys-and-rooms.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         unordered_set<int> visited;
-        queue<int> q;
-        q.push(0);
+        queue<int> qu;
+
+        qu.push(0);
         visited.insert(0);
 
-        while (!q.empty()) {
-            int curr = q.front();
-            q.pop();
+        while (!qu.empty()) {
+            int curr = qu.front();
+            qu.pop();
 
             for (int neighbour : rooms[curr]) {
                 if (visited.count(neighbour) == 0) {
-                    q.push(neighbour);
+                    qu.push(neighbour);
                     visited.insert(neighbour);
                 }
             }
