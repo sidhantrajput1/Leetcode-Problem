9cad7d2660abf187df47f7b8c010bc52b4aadcf0
Sidhant  Rajput <151880795+sidhantrajput1@users.noreply.github.com>
2025-08-27 19:30:35 +0530

    Time: 0 ms (100%), Space: 12.2 MB (30.13%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -1,41 +1,53 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
 class Solution {
 public:
-    unordered_map<Node*, Node*> mp;
+    vector<Node*> nodeRegister;
 
-    void DFS(Node* node, Node* clone_node) {
+    void dfs(Node* actual, Node* clone) {
+        for(auto neighbor : actual->neighbors) {
+            if (!nodeRegister[neighbor->val]) {
+                // create the neighbor for the first neighbor
+                Node* newNode = new Node(neighbor->val);
+                nodeRegister[newNode->val] = newNode;
 
-        for (Node* n : node->neighbors) {
-            if (mp.find(n) == mp.end()) {
-
-                Node* clone = new Node(n->val);
-                mp[n] = clone;
-                clone_node->neighbors.push_back(clone);
-
-                DFS(n, clone);
-
-            }
-            else {
-                clone_node->neighbors.push_back(mp[n]);
+                clone->neighbors.push_back(newNode);
+                dfs(neighbor, newNode);
+            } else {
+                clone->neighbors.push_back(nodeRegister[neighbor->val]);
             }
         }
     }
 
     Node* cloneGraph(Node* node) {
-        if (!node) return nullptr;
+        if (node == nullptr) return nullptr;
 
-        mp.clear();
+        Node* clone = new Node(node->val);
+        nodeRegister.resize(110, nullptr);
 
-        // clone the given node
-        Node* clone_node = new Node(node->val);
+        nodeRegister[clone->val] = clone;
 
-        // Now, clone its neighbour and recursively their neighbour
-        // but if node reappears, then we need to access the clon node 
-        // So store them in a map <Node*, Node*>
+        dfs(node, clone);
+        return clone;
 
-        mp[node] = clone_node;
-
-        DFS(node, clone_node);
-
-        return clone_node;
     }
 };
