cef1b5894eb646d4c66b8ebc8ccdf77c2922b623
Sidhant  Rajput <151880795+sidhantrajput1@users.noreply.github.com>
2025-09-03 17:35:48 +0530

    Time: 10 ms (46.19%), Space: 11.4 MB (24.02%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        unordered_set<ListNode*> visited; // store visited nodes
+        // Edge case: if list is empty or has only one node → no cycle possible
+        if (!head || !head->next) return nullptr;
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        // Detect Cycle
+        while(fast && fast->next) {
+            slow = slow->next; // one step
+            fast = fast->next->next; // two step
 
-        ListNode* current = head;
-        while(current) {
-            // If the node is already visited, cycle start found
-            if (visited.find(current) != visited.end())  return current;
-
-            visited.insert(current);
-            current = current->next;
+            // if slow and fast pointer meet then break the loop
+            if (slow == fast) break;
         }
 
-        return nullptr;
+        // no cycle detect
+        if (slow != fast) return nullptr;
+
+        // find start of cycle
+        ListNode* temp = head;
+        while (temp != slow) {
+            slow = slow->next;
+            temp = temp->next;
+        }
+
+        return slow; // start of cycle
     }
 };
