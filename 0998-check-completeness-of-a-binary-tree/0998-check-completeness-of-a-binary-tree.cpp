d9598d71739ebcda60caef1ee609bea30f682ef4
Sidhant  Rajput <151880795+sidhantrajput1@users.noreply.github.com>
2025-09-12 08:56:34 +0530

    Time: 0 ms (100%), Space: 13.8 MB (53.07%) - LeetHub

--- 0998-check-completeness-of-a-binary-tree/0998-check-completeness-of-a-binary-tree.cpp
+++ 0998-check-completeness-of-a-binary-tree/0998-check-completeness-of-a-binary-tree.cpp
@@ -5,18 +5,21 @@ public:
         queue<TreeNode*> q;
         q.push(root);
 
-        bool past = false;
+        bool past = false; // check if any past node is true or false
 
         while (!q.empty()) {
             auto node = q.front();
             q.pop();
 
+            // if node is find null then mark past equal to true
             if (node == NULL) {
                 past = true;
             } else {
+                // if any past node is null then return false because tree is a
+                // not complete binary tree
                 if (past == true) {
                     return false;
-                } 
+                }
 
                 q.push(node->left);
                 q.push(node->right);
