c46c485b1c6649bdf9f05501fc604f509967bb0a
Sidhant  Rajput <151880795+sidhantrajput1@users.noreply.github.com>
2025-11-11 08:50:00 +0530

    Time: 239 ms (91.02%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -4,3 +4,8 @@ SELECT (
     ORDER BY salary DESC
     lIMIT 1 OFFSET 1
 ) AS SecondHighestSalary;
+
+-- DISTINCT means return only unique value 
+-- ORDER BY salary DESC means sort the salary in descending form like 5, 4, 3, 2, 1
+-- LIMIT 1 means return one first row of table 
+-- OFFSET 1 means skip the first 1 row return the rest of row 
