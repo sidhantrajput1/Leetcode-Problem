0b915085da15a798fd7286966ee0a7f76a9d42c5
Sidhant  Rajput <151880795+sidhantrajput1@users.noreply.github.com>
2025-11-23 12:14:55 +0530

    Time: 0 ms (100%), Space: 9.3 MB (64.55%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    int helper(int sr, int sc, int er, int ec, vector<vector<int>>& dp) {
-        // case : index out of bound
+    int solve(int sr, int sc, int er, int ec, vector<vector<int>>& dp) {
         if (sr > er || sc > ec) return 0;
-
-        // if reached the destination, return 1;
         if (sr == er && sc == ec) return 1;
 
         if (dp[sr][sc] != -1) return dp[sr][sc];
+        
+        int rightStep = solve(sr, sc + 1, er, ec, dp);
+        int downStep = solve(sr + 1, sc, er, ec, dp);
 
-        return dp[sr][sc] = helper(sr + 1, sc, er, ec, dp) + helper(sr, sc+1, er, ec, dp);
+        return dp[sr][sc] = rightStep + downStep;
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, -1)); 
-        return helper(0, 0, m - 1, n - 1, dp);
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return solve(0, 0, m - 1, n - 1, dp);
     }
 };
