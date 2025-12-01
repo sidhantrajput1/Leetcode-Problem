c20936e220d1c95e9b42a26d8ec3ff7d322fdc81
Sidhant  Rajput <151880795+sidhantrajput1@users.noreply.github.com>
2025-12-01 13:55:04 +0530

    Time: 0 ms (100%), Space: 10 MB (89.17%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    int dp[100];
-    int helper(vector<int>& nums, int i) {
-        if (i >= nums.size()) return 0;
-
+    int n;
+    int helper(vector<int>& nums, vector<int>& dp, int i) {
+        if (i >= n) return 0;
+        
         if (dp[i] != -1) return dp[i];
-
-        int take = nums[i] + helper(nums, i + 2);
-        int not_take = helper(nums, i + 1);
-
-        return dp[i] = max(take, not_take);
+        
+        int loot = nums[i] + helper(nums, dp, i + 2);
+        int no_loot = 0 + helper(nums, dp, i + 1);
+        
+        return dp[i] = max(loot, no_loot);
     }
-    int rob(vector<int>& nums) {
-        memset(dp, -1, sizeof(dp));
 
-        return helper(nums, 0);
+    int rob(vector<int>& nums) {
+        n = nums.size();
+        vector<int> dp(105, -1);
+        return helper(nums, dp, 0);
     }
 };
