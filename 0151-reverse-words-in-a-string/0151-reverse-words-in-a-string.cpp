0645b5de338106a10430badc903a49dd2d592f0c
Sidhant  Rajput <151880795+sidhantrajput1@users.noreply.github.com>
2025-12-22 10:10:09 +0530

    Time: 2 ms (44.71%), Space: 11 MB (36.6%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,25 +1,55 @@
 class Solution {
 public:
-    string reverseWords(string s) {
-        
+    // Convert string into array of words
+    vector<string> convertInArr(string s) {
         vector<string> arr;
-        stringstream ss(s);
-        string str;
+        string word = "";
 
-        while(ss>>str){
-            arr.push_back(str);
-            cout<<str<<" ";
+        for (char c : s) {
+            if (c == ' ') {
+                if (!word.empty()) {
+                    arr.push_back(word);
+                    word = "";
+                }
+            } else {
+                word += c;
+            }
         }
 
-        reverse(begin(arr),end(arr));
-
-        string ans="";
-        for(auto &ele : arr){
-            string temp = ele + " ";
-            ans.append(temp);
+        if (!word.empty()) {
+            arr.push_back(word);
         }
 
+        return arr;
+    }
+
+    // Reverse vector<string>
+    void reverseVector(vector<string>& arr, int st, int end) {
+        while (st < end) {
+            string temp = arr[st];
+            arr[st] = arr[end];
+            arr[end] = temp;
+            st++;
+            end--;
+        }
+    }
+
+    string reverseWords(string s) {
+        // Step 1: Convert string to array
+        vector<string> arr = convertInArr(s);
+
+        // Step 2: Reverse the array
+        reverseVector(arr, 0, arr.size() - 1);
+
+        // Step 3: Convert array back to string
+        string ans = "";
+        for (string &word : arr) {
+            ans += word + " ";
+        }
+
+        // Remove trailing space
         ans.pop_back();
+
         return ans;
     }
 };
