d33707c98d9f43ea4ade7d994d72676235e24911
Sidhant  Rajput <151880795+sidhantrajput1@users.noreply.github.com>
2025-12-27 14:11:04 +0530

    Time: 0 ms (100%), Space: 25.8 MB (92.69%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,46 +1,22 @@
 class Solution {
 public:
-    void display(vector<int>& height) {
-        for(int i = 0; i < height.size(); i++) {
-            cout<<height[i]<<" ";
-        }
-        cout<<" ";
-    }
     int trap(vector<int>& height) {
         int n = height.size();
+        int left = 0, right = n - 1;
+        int leftMax = 0, rightMax = 0;
+        int ans = 0;
 
-        //prev greatest element
-        vector<int> prev(n);
-        prev[0] = -1;
-        int max = height[0];
-        for(int i = 1; i < n; i++) {
-            prev[i] = max;
-            if(max < height[i]) max = height[i];
-        }
-
-        // next greatest element
-        vector<int> next(n);
-        next[n-1] = -1;
-        max = height[n-1];
-        for(int i = n-2; i >= 0; i--) {
-            next[i] = max;
-            if(max < height[i]) max = height[i];
-        }
-
-        // find minimum value  // Minimum array
-        vector<int> mini(n);
-        for(int i = 0; i < n; i++) {
-            mini[i] = min(prev[i], next[i]); 
-        }
-
-        // Calculting array
-        int water = 0;
-        for(int i = 1; i < n-1; i++) {
-            if(height[i] < mini[i]) {
-                water += (mini[i] - height[i]);
+        while (left < right) {
+            if (height[left] < height[right]) {
+                leftMax = max(leftMax, height[left]);
+                ans += leftMax - height[left];
+                left++;
+            } else {
+                rightMax = max(rightMax, height[right]);
+                ans += rightMax - height[right];
+                right--;
             }
         }
-
-        return water;
+        return ans;
     }
 };
