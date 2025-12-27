f61bf2f1c66d46598c809665b3a318741c5a1eeb
Sidhant  Rajput <151880795+sidhantrajput1@users.noreply.github.com>
2025-12-27 14:50:40 +0530

    Time: 0 ms (100%), Space: 27.3 MB (28.96%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
+    vector<int> getLeftMaxArray(vector<int>& height, int n) {
+        vector<int> leftMax(n);
+        leftMax[0] = height[0];
+
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = max(leftMax[i - 1], height[i]);
+        }
+        return leftMax;
+    }
+
+    vector<int> getRightMaxArray(vector<int>& height, int n) {
+        vector<int> rightMax(n);
+        rightMax[n - 1] = height[n - 1];
+
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = max(rightMax[i + 1], height[i]);
+        }
+        return rightMax;
+    }
+
     int trap(vector<int>& height) {
         int n = height.size();
-        int left = 0, right = n - 1;
-        int leftMax = 0, rightMax = 0;
-        int ans = 0;
+        if (n == 0) return 0;
 
-        while (left < right) {
-            if (height[left] < height[right]) {
-                leftMax = max(leftMax, height[left]);
-                ans += leftMax - height[left];
-                left++;
-            } else {
-                rightMax = max(rightMax, height[right]);
-                ans += rightMax - height[right];
-                right--;
-            }
+        vector<int> leftMax  = getLeftMaxArray(height, n);
+        vector<int> rightMax = getRightMaxArray(height, n);
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            int water = min(leftMax[i], rightMax[i]) - height[i];
+            if (water > 0)
+                sum += water;
         }
-        return ans;
+        return sum;
     }
 };
