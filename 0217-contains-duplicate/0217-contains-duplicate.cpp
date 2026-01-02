450813f61cf7d04176f13bbaa43fe0c7306fe20d
Sidhant  Rajput <151880795+sidhantrajput1@users.noreply.github.com>
2026-01-02 22:34:01 +0530

    Time: 52 ms (44.71%), Space: 90.9 MB (44.06%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-       unordered_map<int,int> mp;
-       for(int ele:nums){
-            mp[ele]++;
-       }
-       for(const auto& ele : mp){
-            if(ele.second >=2) return true;
-       } 
-       return false;
+        unordered_set<int> st;
+        for (int num : nums) {
+            if (st.find(num) != st.end()) return true;
+            st.insert(num);
+        }
+
+        return false;
     }
 };
